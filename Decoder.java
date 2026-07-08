c484ea2d821bc3b65dd3483306b0a136b1c1427d
brandon dickson <dicksonbrandon101@gmail.com>
2026-07-08 12:25:56 +0100

    made it work with more doc links, if the http link format included javascript it changes to exclude the javascript

--- Decoder.java
+++ Decoder.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.io.File;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,6 +16,18 @@ import org.jsoup.select.Elements;
  */
 public class Decoder
 {
+    public static void main(String[] args){
+        for(String str : args){
+            printData(str);
+        }
+    }
+    public static void test(){
+        printData("https://docs.google.com/document/d/1m5zw3lyK_ZyaRxLBdB9mlCr-KpT_foiuXH-8m-fKhnw/edit?tab=t.0");
+    }
+    public static String convertToJsoupUrl(String originalUrl) {
+        // Finds the document ID and replaces everything after it with the HTML export command
+        return originalUrl.replaceAll("/edit.*$", "/export?format=html");
+    }
     /**
      * takes a URL which has a table of data in the format x, charecter, y, and prints it to the terminal, 
      * 0,0 is bottom left of the image.
@@ -22,26 +35,41 @@ public class Decoder
      */
     public static void printData(String url)
     {
+        boolean ishttp = url.contains("http");
+        if(ishttp){
+            url = convertToJsoupUrl(url);
+        }
         HashMap<Position,String> dataMap = new HashMap<Position,String>();
         
         int maxWidth = 0;
         int maxHeight = 0;
         
         Document doc = null;
+        System.out.println("starting parsing...");
         
         try
         {
-            doc = Jsoup.connect(url).get();
+            if(!ishttp){
+                doc = Jsoup.parse(new File(url));
+                //System.out.println("is file: "+doc.text());
+            }
+            else
+            {
+                doc = Jsoup.connect(url).get();
+            }
+            System.out.println("doc loaded status: " + (doc==null?"null":"good"));
         }
         catch (java.io.IOException ioe)
         {
             ioe.printStackTrace();
             System.out.println("IOException occured, double check the url");
         }
+        //System.out.println("doc:"+doc.text());
         
         boolean isFirstRow = true;
         for(Element row : doc.select("tr"))
         {
+            //System.out.println("printing row :" + row);
             //skip header row, first row is always first
             if(isFirstRow)
             {
